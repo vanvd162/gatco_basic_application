eadc793233c4c2a944e327fb42ae69f191884608
vanvd162 <78011600+vanvd162@users.noreply.github.com>
2022-03-14 14:39:45 +0700

    Update config.py

--- application/config.py
+++ application/config.py
@@ -1,7 +1,7 @@
 class Config(object):
     DEBUG = True
     STATIC_URL = "static"
-    SQLALCHEMY_DATABASE_URI = 'postgresql://gatco_exuser:123456@localhost:5432/gatco_example_app'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Dinhvan99@192.168.0.126:5432/students'
     AUTH_LOGIN_ENDPOINT = 'login'
     AUTH_PASSWORD_HASH = 'sha512_crypt'
     AUTH_PASSWORD_SALT = 'ruewhndjsa17heaw'
