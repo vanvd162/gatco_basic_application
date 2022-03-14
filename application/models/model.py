5ca2707f6bbbbff2ed28a78dfb2008938a5e703e
vanvd162 <78011600+vanvd162@users.noreply.github.com>
2022-03-14 15:21:12 +0700

    Update model.py

--- application/models/model.py
+++ application/models/model.py
@@ -49,66 +49,66 @@ class User(CommonModel):
         """ Show user object info. """
         return '<User: {}>'.format(self.id)
 
-class QuocGia(CommonModel):
-    __tablename__ = 'quocgia'
-    id = db.Column(Integer, primary_key=True)
-    ma = db.Column(String(255), unique=True)
-    ten = db.Column(String(255), nullable=False)
-    mota = db.Column(String(255), nullable=True)
-    tinhthanh = db.relationship("TinhThanh", order_by="TinhThanh.id", cascade="all, delete-orphan")
+# class QuocGia(CommonModel):
+#     __tablename__ = 'quocgia'
+#     id = db.Column(Integer, primary_key=True)
+#     ma = db.Column(String(255), unique=True)
+#     ten = db.Column(String(255), nullable=False)
+#     mota = db.Column(String(255), nullable=True)
+#     tinhthanh = db.relationship("TinhThanh", order_by="TinhThanh.id", cascade="all, delete-orphan")
     
-class TinhThanh(CommonModel):
-    __tablename__ = 'tinhthanh'
-    id = db.Column(Integer, primary_key=True)
-    ma = db.Column(String(255), unique=True)
-    ten = db.Column(String(255), nullable=False)
-    quocgia_id = db.Column(Integer, ForeignKey('quocgia.id'), nullable=False)
-    quocgia = db.relationship('QuocGia')
+# class TinhThanh(CommonModel):
+#     __tablename__ = 'tinhthanh'
+#     id = db.Column(Integer, primary_key=True)
+#     ma = db.Column(String(255), unique=True)
+#     ten = db.Column(String(255), nullable=False)
+#     quocgia_id = db.Column(Integer, ForeignKey('quocgia.id'), nullable=False)
+#     quocgia = db.relationship('QuocGia')
 
-class KhachHang(CommonModel):
-    __tablename__ = 'khachhang'
-    id = db.Column(Integer, primary_key=True)
-    ma = db.Column(String(255), unique=True)
-    ten = db.Column(String(255), nullable=False)
-    quocgia_id = db.Column(Integer, ForeignKey('quocgia.id'), nullable=False)
-    quocgia = db.relationship('QuocGia')
-    sodienthoai = db.Column(String(255))
-    email = db.Column(String(255))
-    diachi = db.Column(String(255))
+# class KhachHang(CommonModel):
+#     __tablename__ = 'khachhang'
+#     id = db.Column(Integer, primary_key=True)
+#     ma = db.Column(String(255), unique=True)
+#     ten = db.Column(String(255), nullable=False)
+#     quocgia_id = db.Column(Integer, ForeignKey('quocgia.id'), nullable=False)
+#     quocgia = db.relationship('QuocGia')
+#     sodienthoai = db.Column(String(255))
+#     email = db.Column(String(255))
+#     diachi = db.Column(String(255))
 
-class HangHoa(CommonModel):
-    __tablename__ = 'hanghoa'
-    id = db.Column(Integer, primary_key=True)
-    ma = db.Column(String(255), unique=True)
-    ten = db.Column(String(255), nullable=False)
-    gia = db.Column(Integer)
-    ghichu = db.Column(String(255))
+# class HangHoa(CommonModel):
+#     __tablename__ = 'hanghoa'
+#     id = db.Column(Integer, primary_key=True)
+#     ma = db.Column(String(255), unique=True)
+#     ten = db.Column(String(255), nullable=False)
+#     gia = db.Column(Integer)
+#     ghichu = db.Column(String(255))
 
-class HoaDon(CommonModel):
-    __tablename__ = 'hoadon'
-    id = db.Column(Integer, primary_key=True)
-    ma = db.Column(String(255), unique=True)
-    ghichu = db.Column(String(255))
-    khachhang_id = db.Column(Integer, ForeignKey('khachhang.id'), nullable=False)
-    tenkhachhang = db.Column(String(255))
-    ngaymua = db.Column(DateTime)
+# class HoaDon(CommonModel):
+#     __tablename__ = 'hoadon'
+#     id = db.Column(Integer, primary_key=True)
+#     ma = db.Column(String(255), unique=True)
+#     ghichu = db.Column(String(255))
+#     khachhang_id = db.Column(Integer, ForeignKey('khachhang.id'), nullable=False)
+#     tenkhachhang = db.Column(String(255))
+#     ngaymua = db.Column(DateTime)
 
-    thanhtien = db.Column(DECIMAL)
-    vat = db.Column(Integer, default=10)
-    tongtien = db.Column(DECIMAL)
+#     thanhtien = db.Column(DECIMAL)
+#     vat = db.Column(Integer, default=10)
+#     tongtien = db.Column(DECIMAL)
 
-    chitiethoadon = db.relationship("ChiTietHoaDon", order_by="ChiTietHoaDon.id", cascade="all, delete-orphan", lazy='dynamic')
+#     chitiethoadon = db.relationship("ChiTietHoaDon", order_by="ChiTietHoaDon.id", cascade="all, delete-orphan", lazy='dynamic')
 
-class ChiTietHoaDon(CommonModel):
-    __tablename__ = 'chitiethoadon'
-    id = db.Column(Integer, primary_key=True)
-    hoadon_id = db.Column(Integer, ForeignKey('hoadon.id'), nullable=False)
+# class ChiTietHoaDon(CommonModel):
+#     __tablename__ = 'chitiethoadon'
+#     id = db.Column(Integer, primary_key=True)
+#     hoadon_id = db.Column(Integer, ForeignKey('hoadon.id'), nullable=False)
 
-    hanghoa_id = db.Column(Integer, ForeignKey('hanghoa.id'), nullable=False)
-    mahanghoa = db.Column(String(255))
-    tenhanghoa = db.Column(String(255))
+#     hanghoa_id = db.Column(Integer, ForeignKey('hanghoa.id'), nullable=False)
+#     mahanghoa = db.Column(String(255))
+#     tenhanghoa = db.Column(String(255))
 
-    soluong = db.Column(Integer)
-    dongia = db.Column(Integer)
-    thanhtien = db.Column(Integer)
+#     soluong = db.Column(Integer)
+#     dongia = db.Column(Integer)
+#     thanhtien = db.Column(Integer)
     
