c253176788eaa93a3f3ed2bdd2f809f3ea5dd477
vanvd162 <78011600+vanvd162@users.noreply.github.com>
2022-03-14 15:23:27 +0700

    Update __init__.py

--- application/controllers/__init__.py
+++ application/controllers/__init__.py
@@ -4,16 +4,17 @@ from application.extensions import jinja
 
 def init_views(app):
     import application.controllers.user
-    import application.controllers.quocgia
-    import application.controllers.tinhthanh
+#     import application.controllers.quocgia
+#     import application.controllers.tinhthanh
 
-    import application.controllers.khachhang
-    import application.controllers.hanghoa
-    import application.controllers.hoadon
+#     import application.controllers.khachhang
+#     import application.controllers.hanghoa
+#     import application.controllers.hoadon
     
     @app.route('/')
     def index(request):
+        return json({"Hello": "Vu dinh van"})
         #return text("Index")
-        return jinja.render('index.html', request)
+#         return jinja.render('index.html', request)
     
     
